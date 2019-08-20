345742c5f01a4ac38161f0c8c636dcc30c055fe7
liuwenjian <liuwenjian091@pingan.com.cn>
2019-08-20 16:15:49 +0800

    添加接口demo

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,8 +8,49 @@ router.get('/', ctx => {
   ctx.body = 'Hello World'
 })
 
-router.get('/api', ctx => {
-  ctx.body = 'api'
+router.post('/api/getIntelligentEvaluateStatus', ctx => {
+  ctx.body = {
+    responseCode: '000000',
+    responseMsg: '成功',
+    data: {
+      barCode: 'KB000900000031',
+      evaluateFlag: '5',
+      evaluateNo: '19082009461100000351'
+    }
+  }
+})
+
+router.post('/api/getIntellEvaOrderStatus', ctx => {
+  ctx.body = {
+    responseCode: '000000',
+    responseMsg: '成功',
+    data: {
+      intellEvaOrderList: [
+        {
+          amount: '405000',
+          productType: '1140100',
+          show: false,
+          status: 2
+        }
+      ]
+    }
+  }
+})
+
+router.post('/api/apply', ctx => {
+  const body = ctx.request.body
+  if (!body.evaluateNo) {
+    ctx.body = {
+      responseCode: '900010',
+      responseMsg: '参数缺失'
+    }
+    return
+  }
+  ctx.body = {
+    responseCode: '000000',
+    responseMsg: '成功',
+    data: null
+  }
 })
 
 module.exports = router
