18d20396ace1bbf360e15f5e27f4d33631156c0c
liuwenjian <liuwenjian091@pingan.com.cn>
2019-08-20 22:22:12 +0800

    auto commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
-module.exports = {
+export default {
   "parserOptions": {
     "ecmaVersion": 6
   }, // 让eslint默认检测es6，避免出现Parsing Error: The keyword 'import' is reserved
+  "sourceType": "module",
   "rules": {
     "indent": ["error", 2],
     "no-console": "off"
