e6a7eefa28e0b2ad26a10453dbf2b24de6cf0153
liuwenjian <liuwenjian091@pingan.com.cn>
2019-08-22 10:17:24 +0800

    auto commit

--- bin/push.sh
+++ bin/push.sh
@@ -6,12 +6,6 @@ if [ ! $hasGit ];then
 else 
   git add .
   git commit -m 'auto commit'
-  git push origin master
-  if [ $? -eq 0 ];then
-    echo 'push成功'
-  else
-    git push github master
-    git push gitee master
-    echo 'push成功'
-  fi
+  git push github master
+  git push gitee master
 fi
