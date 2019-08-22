f1ec68f81870456389565be46322d1e6d9b5a5ab
liuwenjian <liuwenjian091@pingan.com.cn>
2019-08-22 13:20:01 +0800

    auto commit

--- bin/push.sh
+++ bin/push.sh
@@ -4,8 +4,10 @@ if [ ! $hasGit ];then
   echo 'Please download git first!';
   exit 1;
 else 
+  result=`git branch | grep "*"`
+  curBranch=${result:2}
   git add .
   git commit -m 'auto commit'
-  git push github master
-  git push gitee master
+  git push github $curBranch
+  git push gitee $curBranch
 fi
