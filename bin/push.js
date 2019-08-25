6e7d85d82747d7ebf8d807e1499414c0351e2578
luffy <luffybryant@gmail.com>
2019-08-25 22:11:29 +0800

    修改提交脚本

--- bin/push.js
+++ bin/push.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process')
-const commit = spawn('sh', ['./bin/push.sh'])
+const argv = process.argv.slice(2)
+const options = ['./bin/push.sh', ...argv]
+const commit = spawn('sh', options)
 commit.stdout.on('data', data => {
   console.log(`${data}`)
 })
--- bin/push.sh
+++ bin/push.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 hasGit=`which git`
+msg=$1
 if [ ! $hasGit ];then
   echo 'Please download git first!';
   exit 1;
 else 
   result=`git branch | grep "*"`
   curBranch=${result:2}
+  if [ ! $msg ];then
+    msg='auto commit'
+  fi
   git add .
-  git commit -m 'auto commit'
+  git commit -m $msg
   git push github $curBranch
   git push gitee $curBranch
 fi
