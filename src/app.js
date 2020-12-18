26fae4a02aaaa702f899ec65ea7978c80bc590c2
halapro.liu <luffybryant@gmail.com>
2020-12-18 15:37:21 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,52 +10,57 @@ import router from './routers'
 import errorHanlder from './middlewares/error'
 import mongoose from 'mongoose'
 import log from './utils/log'
-import { getIPAddress } from './utils/os'
+import {
+    getIPAddress
+} from './utils/os'
 
 const app = new Koa()
 const port = config.port || '3000' // 端口号
 
 if (config.useMongo) {
-  mongoose.set('useFindAndModify', false)
-  mongoose.set('useCreateIndex', true)
-  mongoose.set('useNewUrlParser', true)
-  mongoose.connect(config.db.url) // 连接mongo
+    mongoose.set('useFindAndModify', false)
+    mongoose.set('useCreateIndex', true)
+    mongoose.set('useNewUrlParser', true)
+    mongoose.connect(config.db.url) // 连接mongo
 }
 
 const middlewares = [
-  convert(cors()),
-  logger(),
-  bodyParser(),
-  serve(path.join(__dirname, config.staticPath)),
-  errorHanlder,
-  router.routes(),
-  router.allowedMethods()
+    convert(cors()),
+    logger(),
+    bodyParser(),
+    serve(path.join(__dirname, config.staticPath)),
+    errorHanlder,
+    // router.routes(),
+    // router.allowedMethods()
 ]
 
 // 挂载中间件
 middlewares.forEach(middleware => {
-  if (!middleware) return
-  app.use(middleware)
+    if (!middleware) return
+    app.use(middleware)
 })
+
+router(app)
+
 
 // 监听错误
 app.on('error', err => {
-  log('cyan', err)
+    log('cyan', err)
 })
 
 // 启动静态服务器
 const server = app.listen(port, _ => {
-  log('green', '************************************')
-  log('green', 'App running at：')
-  log('green', '- Local:   http://localhost:' + port)
-  log('green', '- Network: http://' + getIPAddress() + ':' + port)
-  log('green', '************************************')
+    log('green', '************************************')
+    log('green', 'App running at：')
+    log('green', '- Local:   http://localhost:' + port)
+    log('green', '- Network: http://' + getIPAddress() + ':' + port)
+    log('green', '************************************')
 })
 
 // 监听ctrl+c退出事件
 process.on('SIGINT', () => {
-  log('yellow', 'Stopping dev server')
-  server.close(() => {
-    process.exit(0)
-  })
+    log('yellow', 'Stopping dev server')
+    server.close(() => {
+        process.exit(0)
+    })
 })
